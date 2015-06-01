f61e19f9f93b9e5841ef0f14f86cc4b745bed23e
harley <onyhime@gmail.com>
2015-06-01 22:25:20 +0300

    Configuration fixes

--- migrations/m150602_051709_roles.php
+++ migrations/m150602_051709_roles.php
@@ -12,7 +12,7 @@ class m150602_051709_roles extends Migration
 INSERT INTO r3d.permission (id, name) VALUES ('FILE_READ', 'Может просматривать файлы');
 INSERT INTO r3d.permission (id, name) VALUES ('FILE_WRITE', 'Может загружать файлы');
 INSERT INTO r3d.permission (id, name) VALUES ('GROUP_READ', 'Может просматривать группы файлов');
-INSERT INTO r3d.permission (id, name) VALUES ('GROUP_WRITE', 'Может изменять группы файлов');
+INSERT INTO r3d.permission (id, name) VALUES ('GROUP_WRITE', 'Может изменять группы файлов')
 
 SQL;
 		foreach (explode(";", $sql) as $s) {
